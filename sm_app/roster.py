d0940b1e2254d745e963a4baa30638ae239c71c3
arnav-raja <arnav.raja@arnav.in>
2026-06-15 16:21:18 +0530

    Create roster.py

---
+++ sm_app/roster.py
@@ -0,0 +1,8 @@
+# Sales Manager Salary Roster
+# Add Or Edit Managers Here. Each Name Must Be Unique
+
+SM_ROSTER = {
+    "Anjali Rao":      50000,
+    "Vikram Shetty":   55000,
+    "Meera Joseph":    48000,
+}
